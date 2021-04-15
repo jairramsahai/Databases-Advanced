5b1b58d0c7279e4fc6b23327c07428b778e0fb2a
jairramsahai <71965489+jairramsahai@users.noreply.github.com>
2021-04-15 14:06:33 +0200

    Add files via upload

--- redis_install.sh
+++ redis_install.sh
@@ -1,6 +1,6 @@
 sudo apt update
 sudo apt install redis-server
-sudo nano /etc/redis/redis.conf
+sudo cp /etc/redis/redis.conf /etc/redis.conf.default
 sudo systemctl restart redis.service
 redis-cli
 ping
